301766fc4b1eb2a7d5ed6a4cbd4c25e02edcb379
robinonsay <raomax@gmail.com>
2016-09-17 15:51:46 -0400

    working keyboard

---
+++ app/src/main/java/com/github/faceit/FaceItIME.java
@@ -0,0 +1,90 @@
+package com.github.faceit;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+/**
+ * FaceIt
+ * Created by robinonsay on 9/17/16.
+ */
+public class FaceItIME extends InputMethodService implements KeyboardView.OnKeyboardActionListener{
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+
+    private boolean caps = false;
+
+    @Override
+    public View onCreateInputView() {
+//        return super.onCreateInputView();
+        kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
+        keyboard = new Keyboard(this, R.xml.qwerty);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return kv;
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        switch(primaryCode){
+            case Keyboard.KEYCODE_DELETE :
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                caps = !caps;
+                keyboard.setShifted(caps);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char)primaryCode;
+                if(Character.isLetter(code) && caps){
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code),1);
+        }
+
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
